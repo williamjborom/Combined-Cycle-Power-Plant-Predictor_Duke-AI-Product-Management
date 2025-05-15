14615c76d7fd5531865d14eb85bd2c1339953586
Will Borom <willborom@Wills-MacBook-Air.local>
2025-05-15 05:52:04 -0400

    linear model 2 polynomial

--- combined_cycle_power_plant_predictor.py
+++ combined_cycle_power_plant_predictor.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
 #Download Data
 if not os.path.exists("CCPP_data.csv"):
@@ -26,5 +28,10 @@ lm1 = LinearRegression().fit(X_train, y_train)
 train_accuracy = lm1.score(X_train, y_train)
 print(f"Train accuracy for Linear Model 1 (Basic) is {train_accuracy}")
 
-
+#Linear Model 2
+poly_2_transform = PolynomialFeatures(degree=2)
+X_train_poly2 = poly_2_transform.fit_transform(X_train)
+lm2 = LinearRegression().fit(X_train_poly2, y_train)
+train_accuracy = lm2.score(X_train_poly2, y_train)
+print(f"Train accuracy for Linear Model 2 (Basic) is {train_accuracy}")
 
